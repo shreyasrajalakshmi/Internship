12ba8ca02b6d5e71bf89670d82e52b816cada9cc
shreyasrajalakshmi <shreyas131199@gmail.com>
2025-06-13 21:06:19 +0530

    files updated

--- WEEK 1/May 7/tempCodeRunnerFile.py
+++ WEEK 1/May 7/tempCodeRunnerFile.py
@@ -1,4 +1,5 @@
 if os.path.exists(file_name):
-#     print(f"File '{file_name}' found!")
-# else:
-#     print(f"File '{file_name}' not found.")
+     
+     print(f"File '{file_name}' found!")
+    else:
+     print(f"File '{file_name}' not found.")
